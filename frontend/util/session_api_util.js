28cc9c286b6665eea5cd2c5757c23a212406600f
App Academy Student <robertgoconnor2@gmail.com>
2018-01-02 17:42:41 -0500

    make it better

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -3,7 +3,7 @@ export const signup = user => {
     url: '/api/users',
     method: 'POST',
     data: { user }
-  }).then((user) => console.log(user), error => console.log(error));
+  });
 };
 
 export const login = user => {
@@ -11,12 +11,12 @@ export const login = user => {
     url: '/api/session',
     method: 'POST',
     data: { user }
-  }).then((user) => console.log(user), error => console.log(error));
+  });
 };
 
 export const logout = () => {
   return $.ajax({
     url: '/api/session',
     method: 'DELETE'
-  }).then((user) => console.log(user), error => console.log(error));
+  });
 };
