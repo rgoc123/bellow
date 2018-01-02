33e0ea47ead3f2060c98125482c43715da38ec68
App Academy Student <robertgoconnor2@gmail.com>
2018-01-02 14:45:28 -0500

    Makes user model and user and session routes and controllers

--- frontend/bellow.jsx
+++ frontend/bellow.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.$ = $;
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Bellow!</h1>, root);
 });
