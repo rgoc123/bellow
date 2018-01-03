2fe8bed635bea86c89b412193ebbcff10ac6824d
App Academy Student <robertgoconnor2@gmail.com>
2018-01-02 19:35:50 -0500

    Changes welcome method

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,7 +4,7 @@ const App = () => (
   <div>
     <h1>This. Is. Bellow!</h1>
     <p>This is it for now. Underwhelming. I know.</p>
-    <p>But rest assured. There will be more tomorrow.</p>
+    <p>But rest assured! There will be more tomorrow! Oh yeahhhhh!</p>
   </div>
 );
 
