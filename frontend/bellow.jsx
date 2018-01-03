434e9242c0caa539d92d6c7cec6952932948ead1
App Academy Student <robertgoconnor2@gmail.com>
2018-01-02 19:07:58 -0500

    Creates store and makes import/export fixes to reducers

--- frontend/bellow.jsx
+++ frontend/bellow.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { signup, login, logout } from './util/session_api_util';
+import configureStore from './store/store';
 
+//Testing start
 window.signup = signup;
 window.login = login;
 window.logout = logout;
+//Testing end
 
 document.addEventListener('DOMContentLoaded', () => {
   window.$ = $;
   const root = document.getElementById('root');
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Welcome to Bellow!</h1>, root);
 });
