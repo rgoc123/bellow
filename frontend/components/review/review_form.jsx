c9926e90e3a34534ecd3b838b0f0da678a3a9b86
Robert O'Connor <robertgoconnor2@gmail.com>
2018-01-06 23:40:06 -0500

    Adds placeholder review form component

---
+++ frontend/components/review/review_form.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class ReviewForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>This works</div>
+    );
+  }
+
+}
+
+export default ReviewForm;
