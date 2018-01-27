f5d96b5c9af4adbdbb3592772867959371816f86
Robert O'Connor <robertgoconnor2@gmail.com>
2018-01-26 19:16:00 -0500

    Changes hover styling on business index items

--- frontend/components/business/business_index_item.jsx
+++ frontend/components/business/business_index_item.jsx
@@ -19,9 +19,9 @@ const BusinessIndexItem = ({ business }) => {
     <li className="regular-search-result">
       <div className="biz-listing">
         <div className="main-attributes">
-          <div className="search-result-image"><img src={business.image_url} /></div>
+          <div className="search-result-image"><Link to={`/businesses/${business.id}`}><img src={business.image_url} /></Link></div>
           <div>
-            <Link to={`/businesses/${business.id}`}>{business.name}</Link>
+            <Link className="biz-idx-link" to={`/businesses/${business.id}`}>{business.name}</Link>
             <div className={`rating-img-${business.calculate_rating}`}></div>
             <span>{dollaSign}</span>
           </div>
