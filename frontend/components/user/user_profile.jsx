fe810171eaa450a5c3d163c751a2dc502995870e
Robert O'Connor <robertgoconnor2@gmail.com>
2018-05-02 19:21:33 -0400

    Add positioning to user show page

--- frontend/components/user/user_profile.jsx
+++ frontend/components/user/user_profile.jsx
@@ -3,25 +3,31 @@ import React from 'react';
 class UserProfile extends React.Component {
 
   constructor(props) {
-    debugger
     super(props);
     this.props.fetchUser(this.props.match.params.userId);
   }
 
   render() {
     const user = this.props.user;
-
     debugger
-
     if (!user) {
       return null;
     } else {
+      debugger
+      let ln = user.last_name;
       return (
-        <div className="user-profile">
-          {user.id}
-          {user.first_name}
-          {user.last_name}
-          <img src={user.image} />
+        <div>
+          <div className="grayness-user-prof"></div>
+          <div className="content-container">
+            <div className="user-profile-top">
+              <div className="user-profile-left">
+                <img className="user-profile-image" src={user.image} />
+              </div>
+              <div className="user-profile-right">
+                <h1>{user.first_name} {ln}.</h1>
+              </div>
+            </div>
+          </div>
         </div>
       );
     }
